9b975698de59b195458f585c372d58eaace82c25
hidayatur Romadhan <hidayaturromadhan@gmail.com>
2024-12-28 21:49:27 +0700

    perbaikan title user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,13 +4,13 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - iLanding Bootstrap Template</title>
+  <title>Every Event - Berbagi Kebahagiaan!</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="{{ asset('assets/user/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('assets/user/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ asset('assets/admin/img/logo.png') }}" rel="icon">
+  <link href="{{ asset('assets/admin/img/logo.png') }}" rel="apple-touch-icon">
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
